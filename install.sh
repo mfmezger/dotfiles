07f1a8684c1e970121ce0bae2de1aad60eaae6da
Marc Fabian Mezger <57255687+mfmezger@users.noreply.github.com>
2024-06-06 17:27:07 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,11 @@
 # install brew 
 brew bundle install
 
-
+# install oh my zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+# install powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+# install auto suggestion plugin
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
