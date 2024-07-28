cd5cf9ea084028e05c7c2d10430cca79aa895776
Marc Fabian Mezger <marc.mezger@gmail.com>
2024-07-28 18:28:34 +0200

    new stuff.

---
+++ install_linux.sh
@@ -0,0 +1,13 @@
+# arch based systems
+sudo pacman -Syyu
+
+sudo pacman -S zsh kitty stow atuin eza zoxide docker docker-compose
+
+# Enableling the docker service on system start.
+sudo systemctl start docker.service
+sudo systemctl enable docker.service
+
+sudo usermod -aG docker $USER
+yay -S visual-studio-code-bin
+
+
