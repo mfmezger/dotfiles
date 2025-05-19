e3829ab084f33f07ec9f8fa0af63faa43f18cc29
Marc Fabian Mezger <57255687+mfmezger@users.noreply.github.com>
2025-05-19 08:27:13 +0200

    updates for arch linux.

--- install_linux.sh
+++ install_linux.sh
@@ -1,13 +1,15 @@
 # arch based systems
 sudo pacman -Syyu
 
-sudo pacman -S zsh kitty stow atuin eza zoxide docker docker-compose btop bat 
-
+sudo pacman -Sy zsh kitty stow atuin eza zoxide docker docker-compose btop bat discord onefetch fastfetch neovim  obsidian 
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+yay -S --noconfirm zsh-theme-powerlevel10k-git
+sudo pacman -S github-cli
 # Enableling the docker service on system start.
 sudo systemctl start docker.service
 sudo systemctl enable docker.service
 
 sudo usermod -aG docker $USER
 yay -S visual-studio-code-bin
-
+yay -Sy brave-bin
 
