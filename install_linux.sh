fd3d7aff04052be3d2f36120cb15367f5b17cd4b
Marc Fabian Mezger <57255687+mfmezger@users.noreply.github.com>
2026-01-02 10:17:43 +0100

    fix(Linux): Adding correct flags

--- install_linux.sh
+++ install_linux.sh
@@ -80,7 +80,7 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
     fi
 
     echo ">>> Installing personal AUR packages <<<"
-    sudo pacman -S qbittorrent
+    sudo pacman -S --needed --noconfirm qbittorrent
     yay -S --needed --noconfirm \
         obsidian \
         steam
