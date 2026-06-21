a64a060e9864aac050f5380b8e144e1105c66124
Marc Fabian Mezger <57255687+mfmezger@users.noreply.github.com>
2026-06-21 17:03:06 +0200

    fix(nvim): support neovim 0.12

--- nvim/.config/nvim/lua/lazy_setup.lua
+++ nvim/.config/nvim/lua/lazy_setup.lua
@@ -1,7 +1,7 @@
 require("lazy").setup({
   {
     "AstroNvim/AstroNvim",
-    version = "^4", -- Remove version tracking to elect for nightly AstroNvim
+    version = "^6", -- Track current AstroNvim stable release
     import = "astronvim.plugins",
     opts = { -- AstroNvim options must be set here with the `import` key
       mapleader = " ", -- This ensures the leader key must be configured before Lazy is set up
@@ -17,6 +17,7 @@ require("lazy").setup({
   -- Configure any other `lazy.nvim` configuration options here
   install = { colorscheme = { "astrodark", "habamax" } },
   ui = { backdrop = 100 },
+  rocks = { enabled = false },
   performance = {
     rtp = {
       -- disable some rtp plugins, add more to your liking
